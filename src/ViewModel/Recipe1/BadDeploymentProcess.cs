382bb7a080abaee1f0722acb28b9a20abed17fbe
muen <muen1en5en@gmail.com>
2024-01-08 23:17:07 +0900

    UPDATE: #00003, レシピ1-1コード追加

---
+++ src/ViewModel/Recipe1/BadDeploymentProcess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Oreilly_Csharp_CookBook.ViewModel.Recipe1
+{
+    public class BadDeploymentProcess
+    {
+        StreamWriter report = new StreamWriter("Deployment.txt");
+
+        public bool CheckStatus()
+        {
+            report.WriteLine($"{DateTime.Now} アプリケーションが配置されました");
+
+            return true;
+        }
+    }
+}
---
+++ src/ViewModel/Recipe1/GoodDeploymentProcess.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace Oreilly_Csharp_CookBook.ViewModel.Recipe1
+{
+    public class GoodDeploymentProcess : IDisposable
+    {
+        bool disposed;
+
+        readonly StreamWriter report = new StreamWriter("DeploymentReport.txt");
+
+        public bool CheckStatus()
+        {
+            report.WriteLine($"{DateTime.Now} アプリケーションが配置されました");
+
+            return true;
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // 純粋なマネージリソースはこの位置で破棄する
+                }
+
+                report?.Close();
+                disposed = true;
+            }
+        }
+
+        ~GoodDeploymentProcess()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+
+    // mainメソッド: 呼び出し側
+    //using (var deployer = GoodDeploymentProcess())
+    //{
+    //    deployer.CheckStatus();    
+    //}
+}
