ddb76ca012c7e5cd42e372965c4211dd5709dfa0
muen <muen1en5en@gmail.com>
2024-01-10 23:36:25 +0900

    #0005, recipe1-2コード実装

--- src/ViewModel/Recipe1/DeploymentArtifacts.cs
+++ src/ViewModel/Recipe1/DeploymentArtifacts.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace Oreilly_Csharp_CookBook.ViewModel.Recipe1
 {
-    class DeploymentArtifacts
+    public class DeploymentArtifacts
     {
+        public void Validate()
+        {
+            System.Console.WriteLine("検証中...");
+        }
+    }
+
+    public class DeploymentRepository
+    {
+        public void SaveStatus(string status)
+        {
+            System.Console.WriteLine("ステータスを保存中...");
+        }
+    }
+
+    interface IDeploymentService
+    {
+        void PerformValidation();
+    }
+
+    public class DeploymentService : IDeploymentService
+    {
+        readonly DeploymentArtifacts artifacts;
+        readonly DeploymentRepository repository;
+
+        public DeploymentService(
+            DeploymentArtifacts artifacts,
+            DeploymentRepository repository)
+        {
+            this.artifacts = artifacts;
+            this.repository = repository;
+        }
+
+        public void PerformValidation()
+        {
+            artifacts.Validate();
+            repository.SaveStatus("Status");
+        }
     }
 }
---
+++ src/ViewModel/Recipe1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Oreilly_Csharp_CookBook.ViewModel.Recipe1
+{
+    class Program
+    {
+        readonly IDeploymentService service;
+
+        public Program(IDeploymentService service)
+        {
+            this.service = service;
+        }
+
+        static void Main()
+        {
+            var services = new ServiceCollection();
+
+            services.AddTransient<DeploymentArtifacts>();
+            services.AddTransient<DeploymentRepository>();
+            services.AddTransient<IDeploymentService, DeploymentService>();
+
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            IDeploymentService deploymentService = serviceProvider.GetRequiredService<IDeploymentService>();
+
+            var program = new Program(deploymentService);
+
+            program.StartDeployment();
+        }
+
+        public void StartDeployment()
+        {
+            service.PerformValidation();
+            Console.WriteLine("検証が完了 - 処理を継続します...");
+        }
+    }
+}
