3666ea60e05e0e44eda940d28a5707ae5d4366bb
muen <muen1en5en@gmail.com>
2024-01-12 00:01:39 +0900

    UPDATE: #6, recipe1-2 解説記載

--- src/ViewModel/Recipe1/Program.cs
+++ src/ViewModel/Recipe1/Program.cs
@@ -14,11 +14,20 @@ namespace Oreilly_Csharp_CookBook.ViewModel.Recipe1
 
         static void Main()
         {
+            // IoCコンテナインスタンス化
             var services = new ServiceCollection();
 
+            // 依存する型の追加
+            // 型のリクエストに応じてインスタンスを生成する
             services.AddTransient<DeploymentArtifacts>();
             services.AddTransient<DeploymentRepository>();
             services.AddTransient<IDeploymentService, DeploymentService>();
+
+            // 同じインスタンスを使いまわす場合
+            // services.AddSingleton<DeploymentArtifacts>();
+
+            // オブジェクトの生存期間を細かく管理する場合
+            // services.AddScoped<DeploymentArtifacts>();
 
             ServiceProvider serviceProvider = services.BuildServiceProvider();
 
