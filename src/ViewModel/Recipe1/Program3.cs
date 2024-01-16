038fc82e041eb12bbf10bacf5e8885ec6affb4fa
muen <muen1en5en@gmail.com>
2024-01-17 00:13:10 +0900

    UPDATE: #9, recipe1-3写経

---
+++ src/ViewModel/Recipe1/Program3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oreilly_Csharp_CookBook.ViewModel.Recipe1
+{
+    /// <summary>
+    /// Recipe1-3
+    /// </summary>
+    public class Program3
+    {
+        readonly ThirdPartyDeploymentService service;
+
+        /// <summary>
+        /// ファクトリークラスを用いてサードパーティーオブジェクトを生成
+        /// </summary>
+        /// <param name="factory">ファクトリークラス</param>
+        public Program3(IValidatorFactory factory)
+        {
+            service = factory.CreateDeploymentService();
+        }
+
+        static void Main()
+        {
+            var factory = new ValidatorFactory();
+            var program = new Program3(factory);
+            program.PerformValidation();
+        }
+
+        /// <summary>
+        /// サードパーティー製の機能をつかう
+        /// </summary>
+        void PerformValidation()
+        {
+            service.Validate();
+        }
+
+    }
+}
---
+++ src/ViewModel/Recipe1/ThirdPartyDeploymentService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oreilly_Csharp_CookBook.ViewModel.Recipe1
+{
+    /// <summary>
+    /// サードパーティー製のライブラリなど
+    /// </summary>
+    public class ThirdPartyDeploymentService
+    {
+        public void Validate()
+        {
+            Console.WriteLine("検証されました");
+        }
+    }
+
+    /// <summary>
+    /// IoCコンテナと組み合わせるためのインターフェイス
+    /// </summary>
+    public interface IValidatorFactory
+    {
+        ThirdPartyDeploymentService CreateDeploymentService();
+    }
+
+    /// <summary>
+    /// ファクトリー
+    /// </summary>
+    public class ValidatorFactory : IValidatorFactory
+    {
+        public ThirdPartyDeploymentService CreateDeploymentService()
+        {
+            return new ThirdPartyDeploymentService();
+        }
+    }
+}
